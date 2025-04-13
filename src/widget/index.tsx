23920e3c39bd41a83d73c279c674aee62e85fb23
Huespal <Huespal@users.noreply.github.com>
2025-04-13 13:34:20 +0200

    refactor: isolate validation methods

--- src/widget/index.tsx
+++ src/widget/index.tsx
@@ -1,7 +1,12 @@
 import Root from '@/components/Root';
+import {
+  validateLang, validatePrice, validateTheme
+} from '@/core/helpers/validation';
 import { Langs } from '@/core/i18n/types';
-import { Theme, Themes } from '@/core/theme/types';
+import { Theme } from '@/core/theme/types';
 import { createRoot, Root as RootType } from 'react-dom/client';
+
+const containerId = 'split-payment';
 
 let root: RootType | undefined;
 let price: number | undefined;
@@ -17,56 +22,11 @@ const init = () => {
   }
 }
 
-const getContainer = () => {
-  const containerId = 'split-payment';
-  return containerId
-    ? document.getElementById(containerId)
-    : null;
-}
-
-const validatePrice = (price?: string) => {
-  if (!price) {
-    throw new Error('Missing price attribute');
-  }
-
-  const isNumber = !isNaN(Number(price));
-  if (!isNumber) {
-    throw new Error('Price attribute is not a number');
-  }
-  return Number(price);
-}
-
-const validateTheme = (theme?: string) => {
-  if (!theme) {
-    console.warn('Missing theme attribute. Using base theme.');
-    return Themes.base;
-  } else {
-    const isValidTheme = Object.values(Themes).includes(theme as Theme);
-    if (!isValidTheme) {
-      console.warn('Invalid theme attribute. Using base theme.');
-      return Themes.base;
-    }
-    return theme as Theme;
-  }
-}
-
-const validateLang = (lang?: string) => {
-  if (!lang) {
-    console.warn('Missing language attribute. Using \'en\' language.');
-    return Langs.en;
-  }
-
-  if (!Object.values(Langs).includes(lang as Langs)) {
-    console.warn('Invalid language attribute. Using \'en\' language.');
-    return Langs.en;
-  }
-
-  return lang as Langs;
-}
-
 const render = () => {
   try {
-    const container = getContainer();
+    const container = containerId
+      ? document.getElementById(containerId)
+      : null;
 
     if (container) {
       root = root || createRoot(container);
